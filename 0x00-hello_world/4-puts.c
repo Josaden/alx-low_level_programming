aaf5b44e2ba9959784dc4929a0778464249f3298
josaden <tobijoe37@gmail.com>
2022-10-13 12:55:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
-{	char thisfile = "\"Programming is like building a multilingual puzzle";
+{	char thisfile[] = "\"Programming is like building a multilingual puzzle";
 	puts(thisfile);
 	return 0;
 }
