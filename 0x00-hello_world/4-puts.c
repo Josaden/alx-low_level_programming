effff30395675310134663473eb0d756329ee32c
josaden <tobijoe37@gmail.com>
2022-10-13 13:28:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 int main(void) 
-	/*
-	  *This is how to create the main function with void
+
+	/**
+	 * This is Betty style coding c
+	 *
+	  *Description: This is how to create the main function with void
 	  */
 {	
 	char thisfile[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(thisfile);
 	return (0);
 }
