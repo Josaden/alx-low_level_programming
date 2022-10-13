cd57d1647a850cadd9c6a2b73343332a81ba8f1d
josaden <tobijoe37@gmail.com>
2022-10-13 12:59:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main()
+int main(void) /*This is how to create the main function with void*/
 {	char thisfile[] = "\"Programming is like building a multilingual puzzle";
 	puts(thisfile);
-	return 0;
+	return(0};
 }
