cc3832f46f7b5d26c2de30f28ba7422c4bb0ae85
josaden <tobijoe37@gmail.com>
2022-10-17 09:33:28 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+		if ((c == 101) || (x == 133))
+		{}
+		else
+			putchar(x);
+
+	return (0);
+}
