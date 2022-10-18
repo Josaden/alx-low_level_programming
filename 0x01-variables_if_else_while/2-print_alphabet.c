543f7b4d6f9d34af0f23ccc6d2c4d080beae6235
josaden <tobijoe37@gmail.com>
2022-10-18 03:05:36 -0700

    print alphabet a - z with tile 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: zero(0) if successful
+ */
+int main(void)
+{
+	int x, nl;
+	nl = 10;
+
+	for (x = 97; x <= 122; x++)
+		putchar(x);
+
+	putchar(nl);
+	return (0);
+
+}
