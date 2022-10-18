f9fd88b3f27ddfeefd93482141ed1dbb53e5be73
josaden <tobijoe37@gmail.com>
2022-10-18 13:09:49 -0700

    1-alphabet.c git commit -m 1-alphabet.c "

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: zero(0) if sucessful
+ */
+
+int main(void)
+{
+
+	int n;
+
+	for (n = 97; n <= 122; n++)
+		_putchar(n);
+
+	_putchar(10);
+
+	return (0);
+
+}
