e9b4db3577438c164651b8046b4a47b427029137
josaden <tobijoe37@gmail.com>
2022-10-19 13:49:49 -0700

    code to check for uppercase or lowercase letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha -  function declaration
+ * @c: can be any letter or number
+ * Return: 1 if positive and 0 if negative
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97) && (c <= 122))
+	{
+		return (1);
+	}
+	else if ((c >= 65) && (c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
