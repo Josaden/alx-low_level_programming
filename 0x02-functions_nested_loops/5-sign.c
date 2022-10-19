2611ddfdebc5bcffadd2c077b5e924c59171d505
josaden <tobijoe37@gmail.com>
2022-10-19 14:11:35 -0700

    Code to check a number sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - print the sign of a number
+ * @n: number tested
+ *Return: 1 if positive, 0 if zero and -1 if negative
+*/
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		return (1);
+		printf("+");
+	}
+	else if (n == 0)
+	{
+		return (0);
+		printf("0");
+	}
+	else
+	{
+		return (-1);
+		print(-);
+	}
+}
+
