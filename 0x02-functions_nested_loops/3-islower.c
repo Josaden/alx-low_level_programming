211ec4190b5c975b033fcb6acaad2aed96645d64
josaden <tobijoe37@gmail.com>
2022-10-19 13:17:17 -0700

    code to check if a letter is lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,11 +5,12 @@
  * Return: 0 if successful and 1 if unsuccessful
  * @c: can be any lowercase letter
  */
+
 int _islower(int c)
 {
 	if ((c >= 97) && (c <= 122))
 	{
-	       return (0);
+		return (0);
 	}
 	else
 	{
