08929920c5313c8ce9b1bfc9bf66432f6888bf74
josaden <tobijoe37@gmail.com>
2022-10-20 07:42:01 -0700

    function that prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * print_most_numbers - function that prints the numbers, from 0 to 9
- * void: function returns void
  * Return: void
  */
 
@@ -12,7 +11,7 @@ void print_most_numbers(void)
 
 	for (n = 48; n <= 57; n++)
 	{
-		if ((n != 50) || (n != 52))
+		if ((n != 50) && (n != 52))
 			_putchar(n);
 	}
 	_putchar(10);
